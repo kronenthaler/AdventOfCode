bd416c326b9eb2a47ad5034eed3e78f472971006
kronenthaler <kronenthaler@gmail.com>
2020-12-21 12:47:58 +0100

    bt day20

--- 2020/day20.py
+++ 2020/day20.py
@@ -1,9 +1,9 @@
 import re
 import math
+import sys
 
-
-f = open('data/day20-sample.txt', 'r')
-# f = open('data/day20-final.txt', 'r')
+# f = open('data/day20-sample.txt', 'r')
+f = open('data/day20-final.txt', 'r')
 
 tiles = {}
 while True:
@@ -83,17 +83,46 @@ def neighbours(i,j,n):
     return neighbours
 
 
-def bt(G, tile, i, j, n, visited, table):
-    if i >= n and j >= n:
-        return
-
+def check(i, j, n, table, tiles, o):
     next = neighbours(i, j, n)
-    for g, e in G:
+    inc = ((-1, 0), (0, -1), (1, 0), (0, 1))
+    for d in range(0, 2):
+        if next[d] and table[i+inc[d][0]][j+inc[d][1]][1][(d+2) % 4] != o[d]:
+            return False
+    return True
+
+maxPos = 0
+def bt(G, tiles, i, j, n, visited, table):
+    global maxPos
+    if visited.__len__() == n*n:
+        return table
+
+    if maxPos < i * n + j:
+        maxPos = i * n + j
+        print(i, j)
+
+    for g, e in sorted(G.items(), key=lambda t: t[1].__len__()):
+        if g in visited: continue
+        if i - 1 >= 0 and table[i - 1][j][0] not in e: continue
+        if j - 1 >= 0 and table[i][j - 1][0] not in e: continue
+
         for o in tiles[g]:
-            for node in e:
+            # fits on the table[i][j] (id, orientation)
+            # print(table)
+            if check(i, j, n, table, tiles, o):
+                table[i][j] = (g, o)
+                visited.add(g)
+                # print(i, j, g)
+                result = bt(G, tiles, i + ((j+1) // n), (j+1) % n, n, visited, table)
 
+                if result is not None:
+                    return result
 
+                table[i][j] = ('', None)
+                visited.remove(g)
 
+    # no solution
+    return None
 
 def part1(tiles):
     contacts = {}
@@ -106,13 +135,19 @@ def part1(tiles):
                 for j in range(0, t1.__len__()):
                     o2 = t1[j]
                     if o1[0] == o2[2] or o1[2] == o2[0] or o1[1] == o2[3] or o1[3] == o1[1]:
-                        contacts[id] = contacts.get(id, set()) | set([id1]) #, i, j, o1[0] == o2[2], o1[2] == o2[0], o1[1] == o2[3], o1[3] == o1[1]
-                        contacts[id1] = contacts.get(id1, set()) | set([id]) #, j, i, o2[0] == o1[2], o2[2] == o1[0], o2[1] == o1[3], o2[3] == o1[1]
+                        contacts[id] = contacts.get(id, set()) | set([id1])
+                        contacts[id1] = contacts.get(id1, set()) | set([id])
 
     n = int(math.sqrt(tiles.__len__()))
-    table = list([[''*n] for _ in range(0, n)])
-    bt(contacts, tiles, 0, 0, n, set(), table)
-    print(table)
+    table = []
+    for _ in range(0, n):
+        x = []
+        for _ in range(0, n):
+            x.append(('', None))
+        table.append(x)
+    table = bt(contacts, tiles, 0, 0, n, set(), table)
+    [print(x) for x in table]
+    return int(table[0][0][0]) * int(table[0][n-1][0]) * int(table[n-1][0][0]) * int(table[n-1][n-1][0])
 
 print('part1:', part1(rotate(tiles)))
 
