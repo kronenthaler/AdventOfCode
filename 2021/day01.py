528bf6e8b03e6be853cd592773a6ed7495848eb1
Ignacio Calderon <ignacio.calderon@adevinta.com>
2021-12-01 11:06:19 +0100

    2021: day 1

---
+++ 2021/day01.py
@@ -0,0 +1,34 @@
+
+
+#f = open('data/day1-sample.txt', 'r')
+f = open('data/day1-final.txt', 'r')
+
+
+
+def part1(depths):
+    count = 0
+    last = depths[0]
+    for i in range(1, depths.__len__()):
+        if depths[i] - last > 0:
+            count += 1
+        last = depths[i]
+
+    return count
+
+
+def part2(d):
+    w = (d[0], d[1], d[2])
+    seq = [sum(w)]
+
+    for i in range(3, depths.__len__()):
+        w = (w[1], w[2], d[i])
+        seq.append(sum(w))
+
+    return part1(seq)
+
+
+
+depths = [int(x.strip()) for x in f]
+
+print("part1: ", part1(depths))
+print("part2: ", part2(depths))
