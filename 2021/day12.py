2e029397e1245efbc25ffe98cf1f99109b48152c
Ignacio Calderon <ignacio.calderon@adevinta.com>
2021-12-13 13:41:49 +0100

    day 12

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,16 +1,51 @@
 import re
+import copy
+
 
 # f = open('data/day12-sample.txt', 'r')
 f = open('data/day12-final.txt', 'r')
 
 
-def part1(input):
-    pass
+def part1(links):
+    q = [('start', [])]  # <node, path>
+    paths = set()
+    while len(q) != 0:
+        current = q.pop(0)
+        if current[0] == 'end':
+            paths.add(tuple(current[1] + ['end']))
+            continue
+
+        for e in input[current[0]]:
+            if (e.islower() and e not in current[1]) or (not e.islower()):
+                q.insert(0, (e, current[1] + [current[0]]))
+
+    return len(paths)
 
 
 def part2(input):
-    pass
+    q = [('start', [], set(['start']), None)]  # <node, path, visited, repeated item>
+    paths = set()
+    while len(q) != 0:
+        current = q.pop(0)
+        if current[0] == 'end':
+            paths.add(tuple(current[1] + ['end']))
+            continue
+
+        for e in input[current[0]]:
+            if (e.islower() and e not in current[2] and e != 'start') or (not e.islower()):
+                q.insert(0, (e, current[1] + [current[0]], current[2].union(set([e])), current[3]))
+                if current[3] is None:
+                    q.insert(0, (e, current[1] + [current[0]], current[2], e))
+    # [print(p) for p in paths]
+    return len(paths)
 
 
-print('part1: ', part1(input.copy()))
-print('part2: ', part2(input.copy()))
+links = [tuple(l.strip().split('-')) for l in f]
+input = dict()
+for a, b in links:
+    input.setdefault(a, []).append(b)
+    input.setdefault(b, []).append(a)
+
+
+print('part1: ', part1(copy.deepcopy(input)))
+print('part2: ', part2(copy.deepcopy(input)))
