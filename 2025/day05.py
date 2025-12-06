e00210d3f2c1e563aacde48cf7eb6b3e43323ce7
kronenthaler <kronenthaler@gmail.com>
2025-12-06 20:13:43 +0100

    day 05(refactored) - 2025

--- 2025/day05.py
+++ 2025/day05.py
@@ -1,38 +1,26 @@
 from utils import *
 
-# f = open('data/day05-sample.txt', 'r')
-f = open('data/day05-final.txt', 'r')
+f = open('data/day05-sample.txt', 'r')
+# f = open('data/day05-final.txt', 'r')
 
 lines = [l.strip() for l in f]
-ranges = []
-ids = []
-flip = False
-for l in lines:
-    if len(l) == 0:
-        flip = True
-        continue
-    if not flip:
-        ranges.append(tuple(map(int, l.split('-'))))
-    else:
-        ids.append(int(l))
+ranges = [tuple(map(int, l.split('-'))) for l in lines[0:lines.index('')]]
+ids = [int(l) for l in lines[lines.index('')+1:]]
+
 
 def part1(ranges, ids):
-    fresh = 0
-    for id in ids:
-        for min, max in ranges:
-            if min <= id <= max:
-                fresh += 1
-                break
-    return fresh
+    return len(set(id for id in ids for min, max in ranges if min <= id <= max))
+
 
 def part2(ranges):
     for i, range0 in enumerate(ranges):
         for j, range1 in enumerate(ranges[i + 1:], start=i + 1):
             if range0[1] >= range1[0] and range0[0] <= range1[1]:
-                ranges[i] = [1, 0]
-                ranges[j] = [min(range0[0], range1[0]), max(range0[1], range1[1])]
+                ranges[i] = (1, 0)  # generates a -1
+                ranges[j] = (min(range0[0], range1[0]), max(range0[1], range1[1]))
                 break
-    return sum([r[1] - r[0] for r in ranges]) + len(ranges)
+    return sum([r[1] - r[0] + 1 for r in ranges])
+
 
 print("part1: ", part1(ranges, ids))
 print("part2: ", part2(ranges))
